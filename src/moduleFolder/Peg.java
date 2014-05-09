c9985a007ac50804d1e071a1338b6fc55abe1781
zitag <zitag@users.noreply.github.com>
2014-05-09 15:37:58 -0700

    Update Peg.java

--- src/moduleFolder/Peg.java
+++ src/moduleFolder/Peg.java
@@ -1,6 +1,38 @@
 package moduleFolder;
 
 public class Peg {
-    
+    private static String[] color = { "Blue", "Red", "Black", "White", "Green", "Orange", "Pink", "Gray" };
+	
+    //Empty constructor	
+	public Peg() {	
+	}
+	
+	//Overloaded constructor
+	public Peg ( String[] color ) {
+	   this.color = color;
+	}
+	
+    public void setColor ( String[] c ) {
+	   color = c;
+	}   
 
+    public static String[] addColor () {	 
+      for ( int i = 0; i < color.length; i++ ) {	    
+		 System.out.println( color[i] );
+	  }
+	  return color;
+   } 	
+	
+	public String toString() {	     
+		    return ("Peg color :  " + addColor());        
+    }//end method toString
+	
+   
+   /*
+   //remove after testing
+    public static void main ( String[] args ) {
+	   Peg peg = new Peg(color);
+	   peg.toString();
+	}  
+	*/
 }
